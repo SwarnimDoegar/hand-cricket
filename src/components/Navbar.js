913fafcd6a8dae4cf64fa1816381ad4b5a214b25
Vivek <viveksonybcd@gmail.com>
2020-10-05 13:46:32 +0530

    added icons to indicate current player and added color pallet to css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import logo from '../six.png'
-
+import React from "react";
+import logo from "../assets/six.png";
 
 function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark no-gutters">
-            <a className="navbar-brand logo-margin" href = "./index.html">
-                <img src={logo} width="60" height="60" alt="logo" className = "navbar-logo"></img>
-            </a>
-            <a className="navbar-brand" href="./index.html">
-                <p className="navbar-text text-light">Hand Cricket</p>
-            </a>
-        </nav>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg  no-gutters">
+      <a className="navbar-brand logo-margin" href="./index.html">
+        <img
+          src={logo}
+          width="60"
+          height="60"
+          alt="logo"
+          className="navbar-logo"
+        ></img>
+      </a>
+      <a className="navbar-brand" href="./index.html">
+        <p className="navbar-text text-light">Hand Cricket</p>
+      </a>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
